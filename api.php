05d8fd9cc123ade65408349ce578e76fe0f71e90
davidruc <davidarueda18@gmail.com>
2023-05-23 15:55:07 -0500

    feat: :sparkles: 4th excersice
    Solucion of 4th excersice with validations

--- api.php
+++ api.php
@@ -1,35 +1,25 @@
 <?php
-    // 3. Construir el algoritmo para determinar el voltaje de un
-    // circuito a partir de la resistencia y la intensidad de corriente.
-
-   header("Content-Type: application/json; charset:UTF-8");
-   $_DATA = json_decode(file_get_contents("php://input"), true);
-   $METHOD = $_SERVER["REQUEST_METHOD"];
-
-   function validar($arg){
-    if (is_numeric($arg)){
-        return ("$arg volts");
-    } else {
-        return "ERROR";
+    // 4. Construir el algoritmo que solicite el nombre y edad de 3
+    // personas y determine el nombre de la persona con mayor edad.
+    header("Content-Type: application/json; charset:UTF-8");
+    $_DATA = json_decode(file_get_contents("php://input"), true);
+    $METHOD = $_SERVER["REQUEST_METHOD"];
+    $array = array();
+    for ($i=1; $i <= 3 ; $i++) { 
+        $name[$i] = $_DATA['name'.$i];
+        $edad[$i] = $_DATA['edad'.$i];
+        if(is_numeric($edad[$i])){
+            $array[$name[$i]] = $edad[$i];
+        } else {
+            $array[$name[$i]] = "Edad incorrecta";
+        }
     }
-   }
-   
-   function algoritmo(float $resistencia, float $intensidad){ 
-    $voltaje = $resistencia * $intensidad;
-    return $voltaje;
-   } 
-   try {
-    $res = match($METHOD){
-        "POST" => algoritmo(...$_DATA)
-    };
-   }catch (\Throwable $th) {
-    $res = "ERROR";
-   };
-   $mensaje = (array) [
-    "VOLTAJE" => validar($res),
-   ];
-   echo json_encode($mensaje, JSON_PRETTY_PRINT);
-
-   
-
+    $onlyNum = array_filter($array, 'is_numeric');
+    $maxVal = max($onlyNum);
+    $maxKey = array_search($maxVal, $array);
+    $mensaje = array(
+        "mensaje" => "La persona mayor es $maxKey con una edad de $maxVal.",
+        "notas" => $array,
+    );
+    echo json_encode($mensaje, JSON_PRETTY_PRINT);
 ?>

8e1f1b6ead523843ab87efcd39e24d4afb1c7b62
davidruc <davidarueda18@gmail.com>
2023-05-23 23:03:28 -0500

    feat: :sparkles: 10th exercise
    finished the last exercise

--- api.php
+++ api.php
@@ -3,10 +3,12 @@
     // 9. N atletas han pasado a finales en salto triple en los juegos
     // olímpicos femenino de 2022. Diseñe un programa que pida por
     // teclado los nombres de cada atleta finalista y a su vez, sus
-    // marcas del salto en metros. Informar el nombre de la atleta
-    // campeona que se quede con la medalla de oro y si rompió
-    // récord, reportar el pago que será de 500 millones. El récord
-    // esta en 15,50 metros.
+    // 10. Desarrolle un programa cíclico que capture un dato
+    // numérico cada vez, y los vaya acumulando. El programa se
+    // detiene cuando el usuario digita un cero. El programa debe
+    // mostrar: LA SUMATORIA DE LOS VALORES, EL VALOR DEL
+    // PROMEDIO, CUÁNTOS VALORES FUERON DIGITADOS, MAYOR
+    // VALOR Y MENOR VALOR.
 
     header("Content-Type: application/json; charset:UTF-8");
     $_DATA = json_decode(file_get_contents("php://input"), true);
@@ -16,36 +18,28 @@
         return $arg;
     }
     function algoritmo(){
-    global $_DATA;
-    $record = 15.50;
-    $marcas = [];
-    foreach ($_DATA as $elemento) {
-        $nombre = $elemento['nombre'];
-        $marca = ($elemento['marca']);
-        $marcas[] = $marca;
-        if(is_numeric($marca)){
-            if($marca > $record){
-                $mensaje = "$nombre haz roto el anterior record mundial que era $record [Metros]. Ten 500 millones (Se ha establecido un nuevo record)";
-                $record = $marca;
-            } else {
-                $mensaje = "No rompiste el record (es de $record)";
-            }
-            $oro = max($marcas);
-            $indice = array_search($oro, array_column($_DATA, 'marca'));
-            $nombreGanadora = $_DATA[$indice]['nombre'];
-        } else{
-            $mensaje = "Si participaste??";
-            $marca = "Error";
-            
+        global $_DATA;
+        $datos = array();
+        foreach($_DATA as $elemento){
+            $dato = floatval($elemento['num']);
+            (is_numeric($dato))? array_push($datos, $dato) : "Error";
         }
-        
-        
-    }
-    return array(
-        "mensaje" => $mensaje,
-        "Marca de la atleta ($nombre)" => $marca." [Metros]",
-        "Marca del la mejor atleta ($nombreGanadora)" => $oro." [Metros]",
-    );;
+        $suma = array_sum($datos);
+        $numDatos = count($datos);
+        $promedio = $suma / $numDatos;
+        $maxVal = max($datos);
+        $minVal = min($datos);
+        if (in_array(0, $datos)){
+            return "El programa finalizo";
+        } else {
+            return array(
+                "suma" => $suma,
+                "promedio" => $promedio,
+                "numero de datos digitados" => $numDatos,
+                "valor menor" => $minVal,
+                "valor mayor" => $maxVal,
+            );
+        }
     }
     try {
     $res = match($METHOD){
@@ -56,7 +50,8 @@
     };
     $mensaje = (array) [
         "Data" => $_DATA,
-        "Calculos" => validar($res)
+        "Calculos" => validar($res),
+        
     ];
     echo json_encode($mensaje, JSON_PRETTY_PRINT);
 ?>
